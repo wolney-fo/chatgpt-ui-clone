d4ba53195c127ed109e7eec01f959de932023564
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2023-04-24 00:24:34 -0300

    feat: chat history

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,15 +1,29 @@
-import { userName, userPicUrl } from "@/config";
+import { userChatList, userName, userPicUrl } from "@/config";
 import Image from "next/image";
-import { FiPlus, FiUser, FiMoreHorizontal } from "react-icons/fi";
+import { FiPlus, FiMessageSquare, FiUser, FiMoreHorizontal } from "react-icons/fi";
 
 export function SideBar() {
   return (
     <aside className="w-80 bg-[#202123] items-start p-2">
       <nav className="flex h-full w-full flex-1 flex-col">
-        <a className="flex p-3 items-center gap-3 transition-colors duration-200 text-white cursor-pointer text-sm rounded-md border border-white/20 hover:bg-gray-500/10 shrink-0">
+        <a className="flex p-3 mb-2 items-center gap-3 transition-colors duration-200 text-white cursor-pointer text-sm rounded-md border border-white/20 hover:bg-gray-500/10 shrink-0">
           <FiPlus />
           <span>New chat</span>
         </a>
+
+        {userChatList.map((chat) => (
+          <a
+            href=""
+            className="flex p-3 mt-2 items-center gap-3 transition-colors duration-200 text-white text-sm hover:bg-[#343541] rounded-md"
+            key={chat}
+          >
+            <span className="flex w-full flex-row justify-between">
+              <span className="flex items-center gap-3">
+                <FiMessageSquare /> {chat}
+              </span>
+            </span>
+          </a>
+        ))}
 
         <div className="flex-col flex-1 overflow-y-auto">
           <div className="flex flex-col gap-2 pb-2 text-gray-100 text-sm"></div>
--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,9 @@
 export const userName = "Wolney Oliveira";
 export const userPicUrl = "https://github.com/wolney-fo.png";
+
+export const userChatList = [
+    "History of Brazil",
+    "What is Wikipedia",
+    "Learning Typescript",
+    "Types of numbers",
+]
